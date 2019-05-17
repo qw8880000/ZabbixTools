079a2de243c5cbbbbd85a3be69f4a9ff277ab2eb
qw8880000 <qw8880000@126.com>
2019-05-17 14:10:45 +0800

    update

--- zabbix_history_get.py
+++ zabbix_history_get.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
+import re
 import logging
 import argparse
 from pyzabbix import ZabbixAPI, ZabbixAPIException
@@ -17,12 +18,14 @@ if __name__ == "__main__":
     parser.add_argument("-u", "--user", dest="user", help="The zabbix user.", metavar="USER", required=True)
     parser.add_argument("-p", "--password", dest="password", help="The zabbix password.", metavar="PASSWORD", required=True)
     parser.add_argument("--name", dest="name", help="The item name.", required=True)
+    parser.add_argument("--htype", dest="htype", help="The history type.", required=True)
     args = parser.parse_args()
 
     zabbix_server = args.server
     zabbix_user = args.user
     zabbix_password = args.password
     zabbix_item_name = args.name
+    zabbix_history_type = args.htype
 
     #
     # about zabbix
@@ -37,13 +40,21 @@ if __name__ == "__main__":
 
     for item in items:
         try:
-            historys = zapi.history.get(itemids=item["itemid"], history=4, sortfield="clock", sortorder="DESC", limit=1)
-            host = zapi.host.get(hostids=item["hostid"], output=["hostid", "host", "name"])[0]
+            historys = zapi.history.get(itemids=item["itemid"], history=int(zabbix_history_type), sortfield="clock", sortorder="DESC", limit=1)
+            host = zapi.host.get(hostids=item["hostid"], output=["hostid", "host", "name", "status", "available"])[0]
+
+            # 过滤未启用的主机
+            if str(host["status"]) != "0":
+                continue
 
             if len(historys) == 0:
                 logger.info("===> host [%s][%s], value=%s", host["name"], host["host"], "null")
             else:
+                #print host["host"]
                 logger.info("===> host [%s][%s], value=%s", host["name"], host["host"], historys[0]["value"])
+                #if re.search("Windows", historys[0]["value"]) != None:
+                #    logger.info("===> host [%s][%s], value=%s", host["name"], host["host"], historys[0]["value"])
+
         except ZabbixAPIException as e:
             logger.info("------- item [%s] some error", item["itemid"])
             logger.error(e)
