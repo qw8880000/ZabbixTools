7fa92e8949f6365623ef489e04b3cc0e95675efb
qw8880000 <qw8880000@126.com>
2019-05-22 17:07:45 +0800

    update

---
+++ zabbix_hostgroup_create.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+import logging
+import argparse
+import xlrd
+from pyzabbix import ZabbixAPI, ZabbixAPIException
+
+import myutils
+
+logger = myutils.init_logger()
+
+if __name__ == "__main__":
+    #
+    # 参数解析
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", dest="input", help="The input file.", metavar="INPUT_FILE", required=True)
+    parser.add_argument("-s", "--server", dest="server", help="The zabbix server.", metavar="ZABBIX_SERVER", required=True)
+    parser.add_argument("-u", "--user", dest="user", help="The zabbix user.", metavar="USER", required=True)
+    parser.add_argument("-p", "--password", dest="password", help="The zabbix password.", metavar="PASSWORD", required=True)
+    args = parser.parse_args()
+
+    input_file = args.input
+    zabbix_server = args.server
+    zabbix_user = args.user
+    zabbix_password = args.password
+
+    if not os.path.exists(input_file):
+        logger.warning("The input file does not exist: %s", input_file)
+        sys.exit(1)
+
+    if not os.path.isfile(input_file):
+        logger.warning("The input file is not a file: %s", input_file)
+        sys.exit(1)
+
+    #
+    # xlrd
+    workbook = xlrd.open_workbook(input_file)
+    sheet = workbook.sheet_by_index(0)
+
+    #
+    # about zabbix
+    zapi = ZabbixAPI(zabbix_server)
+    zapi.login(zabbix_user, zabbix_password)
+
+    try:
+
+        for rindex in range(1, sheet.nrows):
+            hostgroup = myutils.xlrd_cell_value_getstr(sheet, rindex, 0)
+
+            if hostgroup == "":
+                continue
+
+            hostgroups = zapi.hostgroup.get(filter={"name": hostgroup})
+            if len(hostgroups) == 0:
+                ret = zapi.hostgroup.create(name=hostgroup)
+                logger.info("====> create success, name: %s", hostgroup)
+            else:
+                logger.info("==> already exist, name: %s", hostgroup)
+
+    except ZabbixAPIException as e:
+        logger.error(e)
+        sys.exit()
+
