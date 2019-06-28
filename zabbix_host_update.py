96c80d6b91cfc5c6d318be708912077f0abf799f
qw8880000 <qw8880000@126.com>
2019-06-28 15:23:01 +0800

    [zabbix_host_update] 脚本优化

--- zabbix_host_update.py
+++ zabbix_host_update.py
@@ -10,6 +10,56 @@ import myutils
 
 logger = myutils.init_logger()
 
+g_proxy_dict = {}
+g_group_dict = {}
+g_template_dict = {}
+
+#
+# Get proxy_id by proxy name
+#
+def get_proxy_id(proxy):
+    if proxy in g_proxy_dict:
+        return g_proxy_dict[proxy]
+    else:
+        proxys = zapi.proxy.get(filter={"host": proxy})
+        if len(proxys) == 0:
+            return None
+
+        proxy_id = proxys[0]["proxyid"]
+        g_proxy_dict[proxy] = proxy_id
+        return proxy_id
+
+#
+# Get group_id by group name
+#
+def get_group_id(group):
+    if group in g_group_dict:
+        return g_group_dict[group]
+    else:
+        hostgroups = zapi.hostgroup.get(filter={"name": group})
+        if len(hostgroups) == 0:
+            return None
+
+        group_id = hostgroups[0]["groupid"]
+        g_group_dict[group] = group_id
+        return group_id
+
+#
+# Get template_id by template name
+#
+def get_template_id(template):
+    if template in g_template_dict:
+        return g_template_dict[template]
+    else:
+        templates = zapi.template.get(filter={"host": template})
+        if len(templates) == 0:
+            return None
+
+        template_id = templates[0]["templateid"]
+        g_template_dict[template] = template_id
+        return template_id
+
+
 def add_templateid_to_host(zapi, hostid, new_template_id):
     templateids = []
     templates_old = zapi.template.get(hostids=hostid)
@@ -31,6 +81,17 @@ def add_groupid_to_host(host, new_group_id):
     
     return groupids
 
+def del_groupid_from_host(host, group_id):
+    groupids = []
+
+    for g in host["groups"]:
+        if g["groupid"] == group_id:
+            continue
+        else:
+            groupids.append({"groupid": g["groupid"]})
+
+    return groupids
+
 if __name__ == "__main__":
     #
     # 参数解析
@@ -41,9 +102,10 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--password", dest="password", help="The zabbix password.", metavar="PASSWORD", required=True)
     parser.add_argument("--update-interface", dest="update_interface", help="", action="store_true")
     parser.add_argument("--update-proxy", dest="update_proxy", help="", action="store_true")
-    parser.add_argument("--add-template", dest="add_template", help="")
-    parser.add_argument("--delete-template", dest="delete_template", help="")
-    parser.add_argument("--update-group", dest="update_group", help="", action="store_true")
+    parser.add_argument("--add-template", dest="add_template", help="", action="store_true")
+    parser.add_argument("--del-template", dest="del_template", help="", action="store_true")
+    parser.add_argument("--add-group", dest="add_group", help="", action="store_true")
+    parser.add_argument("--del-group", dest="del_group", help="", action="store_true")
     parser.add_argument("--update-name", dest="update_name", help="", action="store_true")
     parser.add_argument("--update-status", dest="update_status", help="", action="store_true")
     args = parser.parse_args()
@@ -55,8 +117,9 @@ if __name__ == "__main__":
     update_interface = args.update_interface
     update_proxy = args.update_proxy
     add_template = args.add_template
-    delete_template = args.delete_template
-    update_group = args.update_group
+    del_template = args.del_template
+    add_group = args.add_group
+    del_group = args.del_group
     update_name = args.update_name
     update_status = args.update_status
 
@@ -78,31 +141,6 @@ if __name__ == "__main__":
     zapi = ZabbixAPI(zabbix_server)
     zapi.login(zabbix_user, zabbix_password)
 
-    if add_template != None:
-        if add_template == "":
-            logger.warning("Template is empty.")
-            sys.exit(1)
-
-        templates = zapi.template.get(filter={"host": add_template})
-        if len(templates) == 0:
-            logger.warning("Can't find template [%s] to add", add_template)
-            sys.exit(1)
-        else:
-            templateid_add = templates[0]["templateid"]
-            logger.info("the template [%s][%s] to add", add_template, templateid_add)
-    elif delete_template != None:
-        if delete_template == "":
-            logger.warning("Template is empty.")
-            sys.exit(1)
-
-        templates = zapi.template.get(filter={"host": delete_template})
-        if len(templates) == 0:
-            logger.warning("Can't find template [%s] to delete", delete_template)
-            sys.exit(1)
-        else:
-            templateid_delete = templates[0]["templateid"]
-            logger.info("the template [%s][%s] to delete", delete_template, templateid_delete)
-
     for rindex in range(1, sheet.nrows):
         host_name = myutils.xlrd_cell_value_getstr(sheet, rindex, 0)
         host_sofeware = myutils.xlrd_cell_value_getstr(sheet, rindex, 1)
@@ -110,10 +148,12 @@ if __name__ == "__main__":
         host_ip = myutils.xlrd_cell_value_getstr(sheet, rindex, 3)
         host_proxy = myutils.xlrd_cell_value_getstr(sheet, rindex, 4)
         host_group = myutils.xlrd_cell_value_getstr(sheet, rindex, 5)
+        host_template = myutils.xlrd_cell_value_getstr(sheet, rindex, 6)
 
         host_visible_name = u"{0}-{1}".format(host_sofeware , host_ip)
 
         if host_name == "":
+            logger.info("====> host_name is empty.index %d", rindex)
             continue
 
         try:
@@ -132,29 +172,45 @@ if __name__ == "__main__":
                 params["name"] = host_visible_name
 
             # host group
-            if update_group == True:
-                hostgroups = zapi.hostgroup.get(filter={"name": host_group})
-                if len(hostgroups) == 0:
-                    logger.warning("Can't find hostgroup %s.", host_group)
+            if add_group == True:
+                group_id = get_group_id(host_group)
+                if group_id != None:
+                    params["groups"] = add_groupid_to_host(current_host, group_id)
                 else:
-                    gid = hostgroups[0]["groupid"]
-                    params["groups"] = add_groupid_to_host(current_host, gid)
+                    logger.warning("Can't find hostgroup %s.", host_group)
+
+            # host group del
+            if del_group == True:
+                group_id = get_group_id(host_group)
+                if group_id != None:
+                    params["groups"] = del_groupid_from_host(current_host, group_id)
+                else:
+                    logger.warning("Can't find hostgroup %s.", host_group)
 
             # proxy
             if update_proxy == True:
-                proxys = zapi.proxy.get(filter={"host": host_proxy})
-                if len(proxys) != 0:
-                    params["proxy_hostid"] = proxys[0]["proxyid"]
+                proxy_id = get_proxy_id(host_proxy)
+                if proxy_id != None:
+                    params["proxy_hostid"] = proxy_id
+                else:
+                    logger.warning("Can't find proxy %s.", host_proxy)
 
             # template add
-            if add_template != None:
-                templateids = add_templateid_to_host(zapi, hostid, templateid_add)
-                if len(templateids) > 0:
-                    params["templates"] = templateids
+            if add_template == True:
+                template_id = get_template_id(host_template)
+                if template_id != None:
+                    params["templates"] = add_templateid_to_host(zapi, hostid, template_id)
+                else:
+                    logger.warning("Can't find template %s.", host_template)
+
 
             # template delete
-            if delete_template != None:
-                params["templates_clear"] = [{"templateid": templateid_delete}]
+            if del_template != None:
+                template_id = get_template_id(host_template)
+                if template_id != None:
+                    params["templates_clear"] = [{"templateid": template_id}]
+                else:
+                    logger.warning("Can't find template %s.", host_template)
 
             # status
             if update_status == True:
